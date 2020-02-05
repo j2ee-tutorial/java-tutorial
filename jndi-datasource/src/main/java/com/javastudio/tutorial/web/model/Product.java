3a6f54bc4df0e6aae8294bf22e50a20dda88a55c
Hossein Mohammadi <hussein.muhammadii@gmail.com>
2020-02-05 12:42:13 +0330

    product

---
+++ jndi-datasource/src/main/java/com/javastudio/tutorial/web/model/Product.java
@@ -0,0 +1,34 @@
+package com.javastudio.tutorial.web.model;
+
+public class Product {
+
+    Long id;
+
+    String name;
+
+    Integer count;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+}
