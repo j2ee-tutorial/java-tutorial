dc663e894df0356bfc2035b49e77dfa01642c46b
Hossein Mohammadi <hussein.muhammadi@hotmail.com>
2020-02-06 23:57:15 +0330

    Spring MVC

--- spring-mvc/src/main/java/com/javastudio/tutorial/web/controller/HelloWorld.java
+++ spring-mvc/src/main/java/com/javastudio/tutorial/web/controller/HelloWorld.java
@@ -2,12 +2,13 @@ package com.javastudio.tutorial.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloWorld {
-    @RequestMapping("/welcome")
-    public ModelAndView welcome(){
+    @RequestMapping(value = "/welcome", method = RequestMethod.GET)
+    public ModelAndView welcome() {
         String message = "<h1>Hello world!</h1>";
         return new ModelAndView("welcome", "message", message);
     }
