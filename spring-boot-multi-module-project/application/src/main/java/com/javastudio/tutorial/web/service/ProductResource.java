2f8646febda48d888df59b099c4e04c1e4572b78
Hossein Mohammadi <hussein.muhammadii@gmail.com>
2020-02-16 08:39:12 +0330

    Add api module

--- spring-boot-multi-module-project/application/src/main/java/com/javastudio/tutorial/web/service/ProductResource.java
+++ spring-boot-multi-module-project/application/src/main/java/com/javastudio/tutorial/web/service/ProductResource.java
@@ -1,7 +1,7 @@
 package com.javastudio.tutorial.web.service;
 
+import com.javastudio.tutorial.api.ProductService;
 import com.javastudio.tutorial.component.MyComponent;
-import com.javastudio.tutorial.service.ProductService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
--- spring-boot-multi-module-project/service/src/main/java/com/javastudio/tutorial/service/ProductServiceImpl.java
+++ spring-boot-multi-module-project/service/src/main/java/com/javastudio/tutorial/service/ProductServiceImpl.java
@@ -1,12 +1,13 @@
 package com.javastudio.tutorial.service;
 
+import com.javastudio.tutorial.api.ProductService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ProductService {
-    private static final Logger LOGGER = LoggerFactory.getLogger(ProductService.class);
+public class ProductServiceImpl implements ProductService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProductServiceImpl.class);
 
     public void list() {
         LOGGER.info("Populate product list in service layer");
